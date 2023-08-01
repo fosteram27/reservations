ad01074a163e90986e91df68aaa8279daa23fd0f
afoster <fosteram27@gmail.com>
2023-08-01 14:02:06 -0600

    updates after Week 1 Video "SwiftUI scenes and views"

---
+++ reservations/MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  reservations
+//
+//  Created by Andrew Foster on 8/1/23.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text("Little Lemon")
+    }
+}
+
+struct MyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyView()
+    }
+}
