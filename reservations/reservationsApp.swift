e8120bc592f1f196897e22eb778072a4572d73f9
afoster <fosteram27@gmail.com>
2023-08-01 13:10:32 -0600

    Initial Commit

---
+++ reservations/reservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  reservationsApp.swift
+//  reservations
+//
+//  Created by Andrew Foster on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct reservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
